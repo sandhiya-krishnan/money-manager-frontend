ab054cc261fb4654485d565de69f59b3c145af6d
sandhiya-krishnan <sanjukrish2005@gmail.com>
2026-02-06 11:44:09 +0530

    baseurl

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://money-manager-backend-y01u.onrender.com",
+  baseURL: "https://money-manager-backend-y01u.onrender.com/api",
 });
 
 export const fetchTransactions = () => API.get("/transactions");
